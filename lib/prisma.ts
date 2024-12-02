1b37876bb434025fda4b55fa4c7ba62082142912
⚡ MD. Jahid Hasan <mjahidhasand4@gmail.com>
2024-12-02 23:36:14 +0600

    Fixed prisma issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,7 @@ declare global {
 }
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  global.prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
