c31ead6501a1bd66f50cbfd3e1853bd5fce26c74
⚡ MD. Jahid Hasan <mjahidhasand4@gmail.com>
2024-12-03 04:19:54 +0600

    Fixed the cors issue

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,17 @@
 import express, { type Application } from "express";
 import { routes } from "../routes";
+import cors from "cors"
 
 const app: Application = express();
 const port = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
